d539f70252518c7851f3b41c9a74f8df979a9ca2
SWTD 18E <pseude18@gmail.com>
2023-03-12 14:41:54 -0600

    mldkqd

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import password_cracker
 from unittest import main
+import hashlib
 
-cracked_password1 = password_cracker.crack_sha1_hash(
-    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
+cracked_password1 = password_cracker.crack_sha1_hash("2ea6201a068c5fa0eea5d81a3863321a87f8d533")
 print(cracked_password1)
 
 cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
+    "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
 print(cracked_password2)
 
 # Run unit tests automatically
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,29 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def crack_sha1_hash(hash,use_salts=False):
+
+    with open("top-10000-passwords.txt","r") as f:
+        found=False
+        for line in f:
+            if(use_salts):
+                with(open("known-salts.txt"))as f2:
+                    for lin in f2:
+                        preappended=hashlib.sha1((lin.strip()+line.strip()).encode("utf-8")).hexdigest()
+                        appended= hashlib.sha1((line.strip()+lin.strip()).encode("utf-8")).hexdigest()
+                        if hash == preappended or hash==appended:
+                            found=True
+                            break
+                f2.close()        
+                if(found):
+                     break
+            else :
+                if hash == hashlib.sha1(line.strip().encode("utf-8")).hexdigest():
+                    found=True
+                    break 
+        f.close()                
+        if(found):
+            return line.strip()
+        else :
+            f.close()
+            return "PASSWORD NOT IN DATABASE"
+  
